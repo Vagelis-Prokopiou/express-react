ac5dfba97ff672b888bf4a0a6f24c491b242db21
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-10-01 09:24:45 +0300

    Folders/files structure update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const reverseString = require('./helpers').reverseString;
+const reverseString = require('./helpers/helpers').reverseString;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.get('/', (req, res) => res.send('The server is running.'));

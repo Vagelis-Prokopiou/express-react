520837cfefe8f5c983dca321238306a1f2f7f02d
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-10-01 08:57:14 +0300

    C:/Users/Vangelisp/Downloads/programs/PortableGit/api/reverse-string done

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const reverseString = require('./helpers').reverseString;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 app.get('/', (req, res) => res.send('The server is running.'));
 
 app.get('/api/reverse-string', (request, response) =>
 {
-  //
-  response.send('Api page');
+  response.json({'string': reverseString(request.query.string)});
 });

f272c4f0d5e41eb6c7b65500b5ea39e1720634f6
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-10-01 15:09:26 +0300

    Server ready

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const reverseString = require('./helpers/helpers').reverseString;
+const helpers = require('./helpers/helpers');
+const request = require('request');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
 app.get('/', (req, res) => res.send('The server is running.'));
 
-app.get('/api/reverse-string', (request, response) =>
+app.get('/api/reverse-string', (req, res) =>
 {
-  response.json({'string': reverseString(request.query.string)});
+  if (!helpers.isDefined(req.query.string))
+  {
+    res.json({data: 'Missing parameter string.'});
+    return;
+  }
+  res.json({data: helpers.reverseString(req.query.string)});
+});
+
+app.get('/api/data', (req, res) =>
+{
+  // http://jsonplaceholder.typicode.com/posts
+  const url = req.query.url;
+  const method = req.query.method.toUpperCase();
+
+  request({url: url, method: method, json: true}, (error, response, body) =>
+  {
+    if (error)
+    {
+      res.json({data: 'No data retrieved from the external service.'});
+      return;
+    }
+    res.json({data: body});
+  });
 });

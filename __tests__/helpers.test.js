52b98835c3dd46e495b45855a102d5da5b9f59de
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-10-01 13:28:38 +0300

    Adding isDefined()

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,15 +1,31 @@
-const helpers = require('../helpers/helpers');
+const helpers = require('../helpers/helpers')
 
 test('reverseString', () =>
 {
   const tests = [
-    {value: '', expectedValue: ''}
-    , {value: 'hello', expectedValue: 'olleh'}
-    , {value: 'hello world', expectedValue: 'dlrow olleh'},
-  ];
+    {value: '', expectedValue: ''},
+    {value: 'hello', expectedValue: 'olleh'},
+    {value: 'hello world', expectedValue: 'dlrow olleh'}]
 
   tests.forEach(test =>
   {
-    expect(helpers.reverseString(test.value)).toBe(test.expectedValue);
-  });
-});
+    expect(helpers.reverseString(test.value)).toBe(test.expectedValue)
+  })
+})
+
+test('isDefined', () =>
+{
+  const tests = [
+    {value: undefined, expectedValue: false},
+    {value: null, expectedValue: false},
+    {value: false, expectedValue: false},
+    {value: true, expectedValue: true},
+    {value: '', expectedValue: true},
+    {value: {}, expectedValue: true},
+    {value: [], expectedValue: true}]
+
+  tests.forEach(test =>
+  {
+    expect(helpers.isDefined(test.value)).toBe(test.expectedValue)
+  })
+})
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
   'reverseString': function(str) { return str.split('').reverse().join(''); },
+  'isDefined': function(param) { return param !== undefined && param !== null && param !== false; },
 };
 
